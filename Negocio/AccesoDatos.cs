3e880649c5498b39c2a12ccfd28415bb67826672
ezep23 <ezequielpistoneee@gmail.com>
2026-09-11 03:49:22 -0300

    Clase acceso a datos

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            string cadenaConexion = "server=.\\SQLEXPRESS; database=; integrated security=true; TrustServerCertificate=True;";
+            conexion = new SqlConnection(cadenaConexion);
+            comando = new SqlCommand();
+        }
+
+        public void SetearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+            comando.Parameters.Clear();
+
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                abrirConexion();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                abrirConexion();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public object ejecutarScalar()
+        {
+            try
+            {
+                abrirConexion();
+                return comando.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        public void abrirConexion()
+        {
+            if (conexion.State != ConnectionState.Open)
+            {
+                conexion.Open();
+            }
+        }
+
+    }
+}
