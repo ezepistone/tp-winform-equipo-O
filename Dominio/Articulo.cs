d6b27bb2fbdaa9afc888c766d17f193a820e94a9
ezep23 <ezequielpistoneee@gmail.com>
2026-09-11 03:29:34 -0300

    Arquitectura en capas

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Gestión_de_Catálogo
+{
+    public class Articulo
+    {   //datos minimos codigo, nombre, descrip, precio
+        private string codigo;
+        private string nombre;
+        private string descripcion;
+        private double precio;
+
+        public Articulo(string codigo, string nombre, string descripcion, double precio)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+        }
+        //sobrecargo el constructor
+        public Articulo() { }
+
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        public double Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+        ///metodos
+        public string mostrarDetalle()
+        {
+            return "Código: " + codigo + "Nombre: " + nombre + "Descripción: " + descripcion + "Precio: " + precio;
+        }
+    }
+}
+
---
+++ Dominio/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace App_Gestión_de_Catálogo
+{
+    public class Categoria : EntidadBase
+    {
+        public Categoria(int id, string nombre) : base(id, nombre) { }
+        //sobrecargo el constructor
+        public Categoria() { }
+    }
+}
